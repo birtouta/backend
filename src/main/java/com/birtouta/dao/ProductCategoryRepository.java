209ab1be0f15d2efeb214744d7dc66acc506837d
DjafriMouradGit <djafrimourad.jobs@gmail.com>
2020-04-13 14:42:36 +0200

    manage ProductCategory(entity,service and controller)

--- src/main/java/com/birtouta/dao/ProductCategoryRepository.java
+++ src/main/java/com/birtouta/dao/ProductCategoryRepository.java
@@ -1,11 +1,10 @@
 package com.birtouta.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import com.birtouta.entities.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.birtouta.entities.ProductCategory;
-
 @Repository
-public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Long>{
-
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    ProductCategory getById(Long id);
 }
--- src/main/java/com/birtouta/entities/ProductCategory.java
+++ src/main/java/com/birtouta/entities/ProductCategory.java
@@ -1,5 +1,7 @@
 package com.birtouta.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 import lombok.ToString;
 
@@ -12,6 +14,7 @@ import java.util.Objects;
 @Table(name="prodcut_categories")
 @Data
 @ToString
+@JsonIgnoreProperties({"orderProducts"})
 public class ProductCategory implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
@@ -22,7 +25,8 @@ public class ProductCategory implements Serializable {
 	private String translatedEn;
 	private String translatedKb;
 
-	@OneToMany(mappedBy="productCategory" , fetch = FetchType.LAZY,cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy="productCategory" , fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@JsonManagedReference
 	private List<OrderProduct> orderProducts;
 
 	public ProductCategory() {
