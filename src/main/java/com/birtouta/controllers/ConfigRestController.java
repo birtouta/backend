dbd440d4591da731de574045c7f7480492a07af6
magixus <magixusneo@live.fr>
2020-04-16 20:39:35 +0100

    update config and response structed for configuration

--- src/main/java/com/birtouta/controllers/ConfigRestController.java
+++ src/main/java/com/birtouta/controllers/ConfigRestController.java
@@ -11,6 +11,7 @@ import com.birtouta.dao.SessionRepository;
 import com.birtouta.entities.Configuration;
 import com.birtouta.entities.Session;
 import com.birtouta.entities.User;
+import com.birtouta.services.Response;
 
 @RestController
 @RequestMapping(path="/config",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -22,21 +23,33 @@ public class ConfigRestController {
 	SessionRepository sessionRepository ;
 	
 	@PostMapping(path="/add")
-	public ResponseEntity<?> addConfig (@RequestBody Configuration configuration){
-		return new ResponseEntity<Configuration>(configurationRepository.save(configuration), HttpStatus.OK); 
+	public ResponseEntity<?> addConfig (Configuration configuration){
+		return new ResponseEntity<Response>(new Response("Configuration successfully registered !", configurationRepository.save(configuration),  200, true), HttpStatus.OK);
 	}
 	
-	@PostMapping(path="/get")
+	@PostMapping(path="/update")
+	public ResponseEntity<?> updateConfig (Configuration configuration, @RequestHeader("Token") String token){
+		Session session = sessionRepository.findByToken(token);
+		if(session == null) {
+			return new ResponseEntity<Response>(new Response("Unauthorized Token", null,  401, false), HttpStatus.UNAUTHORIZED);
+		}else {
+			
+			Configuration  c = session.getUser().getConfiguration(); 
+			configuration.setId(c.getId());
+			return new ResponseEntity<Response>(new Response("Configuration successfully updated !", configurationRepository.save(configuration),  200, true), HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping(path="/get", consumes = MediaType.ALL_VALUE)
 	public ResponseEntity<?> getConfig (@RequestHeader("Token") String token){
 		Session session = sessionRepository.findByToken(token);
 		if(session == null) {
-			return new ResponseEntity<String>("Unauthorized Token", HttpStatus.UNAUTHORIZED);
+			return new ResponseEntity<Response>(new Response("Unauthorized Token", null,  401, false), HttpStatus.UNAUTHORIZED);
 		}else {
 			
 			User user = session.getUser() ;
 			Configuration  configuration = user.getConfiguration(); 
-			
-			return new ResponseEntity<Configuration>(configuration, HttpStatus.OK);
+			return new ResponseEntity<Response>(new Response("Configuration successfully retreived !", configuration,  200, true), HttpStatus.OK);
 		}
 	}
 	
